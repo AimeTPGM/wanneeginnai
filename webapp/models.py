f5db1a165fcee85fa4c5b0b3e26a2e272f2d0b5f
Pankamol Srikaew <aim.ske09@gmail.com>
2018-05-31 00:05:59 +0700

    add superuser

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Restuarant(models.Model):
+    name = models.CharField(max_length=100)
+
+    def save(self):
+        self.save()
+
+    def __str__(self):
+        return self.name
