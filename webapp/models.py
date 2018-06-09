3d884d2542d57ca9af12000ca9173ac3e9372b8e
Pankamol Srikaew <aim.ske09@gmail.com>
2018-06-09 21:54:29 +0700

    add css

--- webapp/models.py
+++ webapp/models.py
@@ -3,8 +3,5 @@ from django.db import models
 class Restuarant(models.Model):
     name = models.CharField(max_length=100)
 
-    def add(self):
-        self.save()
-
     def __str__(self):
         return self.name
