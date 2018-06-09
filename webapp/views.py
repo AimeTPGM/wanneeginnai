704305a907f17313a875587369e6c3dd597131ae
Pankamol Srikaew <aim.ske09@gmail.com>
2018-06-09 22:19:21 +0700

    refactored

--- webapp/views.py
+++ webapp/views.py
@@ -1,36 +1,50 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .form import RestuarantForm
 from .models import Restuarant
 from random import randint
 
+def goToListPage(request):
+    restuarants = Restuarant.objects.all()
+    return render(request, 'list.html', { 'isShowingInput': True, 'restuarants': restuarants })
+
+def goToRandom(request, firstRandom, showingText):
+    return render(request, 'random.html', { 'isShowingInput': False, 'firstRandom': firstRandom, 'result': showingText })
+
+def goToAddPage(request, form):
+    return render(request, 'add.html', { 'isShowingInput': True, 'form': form, 'isShowingEdit': False })
+
+def save(request, instance):
+    if instance is None:
+        form = RestuarantForm(request.POST)
+    else:
+        form = RestuarantForm(request.POST, instance=instance)
+    if form.is_valid():
+        restuarant = form.save(commit=False)
+        restuarant.save()
+
 def add(request):
     if request.user.is_authenticated:
         if request.method == "POST":
-            form = RestuarantForm(request.POST)
-            if form.is_valid():
-                restuarant = form.save(commit=False)
-                restuarant.save()
-                restuarants = Restuarant.objects.all()
-                return render(request, 'list.html', { 'isShowingInput': True, 'restuarants': restuarants })
+            save(request, None)
+            return goToListPage(request)     
         else: 
             form = RestuarantForm()
-        return render(request, 'add.html', { 'isShowingInput': True, 'form': form, 'isShowingEdit': False })
+        return goToAddPage(request, form)
     else:
-        return render(request, 'random.html', { 'isShowingInput': False, 'firstRandom': True, 'result': 'วันนี้กินไหน' })
+        return redirect('mainpage')
 
 def mainpage(request):  
-    return render(request, 'random.html', { 'isShowingInput': False, 'firstRandom': True, 'result': 'วันนี้กินไหน' })
+    return goToRandom(request, True, 'วันนี้กินไหน')
 
 def random(request):
     restuarantQuerySet = Restuarant.objects.all()    
     randNum = randint(0, restuarantQuerySet.count()-1)
     restuarantList = list(restuarantQuerySet)
     result = restuarantList[randNum]
-    return render(request, 'random.html', { 'isShowingInput': False, 'firstRandom': False, 'result': result })
+    return goToRandom(request, False, result)
 
 def listpage(request):
-    restuarants = Restuarant.objects.all()
-    return render(request, 'list.html', { 'isShowingInput': True, 'restuarants': restuarants })
+    return goToListPage(request)
 
 def delete(request, pk):
     if request.user.is_authenticated:
@@ -38,22 +52,18 @@ def delete(request, pk):
             restuarant = Restuarant.objects.get(id=pk).delete()
         except Restuarant.DoesNotExist:
             print('not existed')
-        restuarants = Restuarant.objects.all()
-        return render(request, 'list.html', { 'isShowingInput': True, 'restuarants': restuarants })
+        return goToListPage(request)
     else:
-        return render(request, 'random.html', { 'isShowingInput': False, 'firstRandom': True, 'result': 'วันนี้กินไหน' })
+        return redirect('mainpage')
 
 def edit(request, pk):
     if request.user.is_authenticated:
         restuarant = get_object_or_404(Restuarant, pk=pk)
         if request.method == "POST":
-            restuarantForm = RestuarantForm(request.POST, instance=restuarant)
-            restuarant = restuarantForm.save(commit=False)
-            restuarant.save()
-            restuarants = Restuarant.objects.all()
-            return render(request, 'list.html', { 'isShowingInput': True, 'restuarants': restuarants })
+            save(request, restuarant)
+            return goToListPage(request)
         else:
             restuarantForm = RestuarantForm(instance=restuarant)
-        return render(request, 'add.html', { 'isShowingInput': True, 'form': restuarantForm, 'isShowingEdit': True })
+        return goToAddPage(request, restuarantForm)
     else:
-        return render(request, 'random.html', { 'isShowingInput': False, 'firstRandom': True, 'result': 'วันนี้กินไหน' })
+        return redirect('mainpage')
